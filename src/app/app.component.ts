3f5660755bf4b22c91dfe06c6c823450c2070556
Waraht Punyarote <rushing.wp@gmail.com>
2023-04-29 09:54:48 +0700

    feat: Add "create" todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-todo';
+  todos: string[] = [];
+  todo = '';
+
+  onUpdateTodo(newValue: string) {
+    this.todo = newValue;
+  }
+
+  onAdd() {
+    if (!this.todo) return;
+    this.todos.push(this.todo);
+    this.todo = '';
+  }
+
+  onChangeTitle() {
+    this.title = 'Angular Todo App';
+  }
 }
